f0d43d328cb0ef23eb4386936a8f48e2da62b75a
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2026-02-25 02:34:00 +0200

    updated path to tournament data in brackets page logic in main statics CDN

--- js-static/pages/brackets.js
+++ js-static/pages/brackets.js
@@ -113,7 +113,8 @@ async function loadTournamentFromParameter() {
 async function fetchTournamentsList() {
     try {
         // Uncomment the line below for production
-        const response = await fetch('https://raw.githubusercontent.com/HEATLabs/HEAT-Labs-Configs/refs/heads/main/tournaments-dev.json');
+        const response = await fetch('https://cdn1.heatlabs.net/tournaments.json');
+        // Uncomment the line below for development
         // const response = await fetch('../HEAT-Labs-Configs/tournaments-dev-local.json');
         if (!response.ok) {
             throw new Error(`Failed to fetch tournaments list: ${response.status}`);

97a5bb2ba0045dc2f8d673f72f0bfaaecc6e43e2
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2026-02-27 12:36:09 +0200

    updated global hero module image count from 61 to 63 in main website statics CDN

--- js-static/modules/global-hero.js
+++ js-static/modules/global-hero.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Only proceed if we found hero elements
     if (heroes.length > 0) {
-        // Generate a random number between 1 and 61
-        const randomImageNumber = Math.floor(Math.random() * 61) + 1;
+        // Generate a random number between 1 and 63
+        const randomImageNumber = Math.floor(Math.random() * 63) + 1;
 
         // Construct the new image URL
         const newImageUrl = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://cdn6.heatlabs.net/hero-background/${randomImageNumber}.webp')`;
